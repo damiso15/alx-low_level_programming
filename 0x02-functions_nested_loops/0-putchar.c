435f23c8ece94c5582a59e1a40ddb33acbc0d7fb
damiso <damiso15@yahoo.com>
2022-12-13 06:30:17 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include  <unistd.h>
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main (void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,10 @@
+# alx-low_level_programming
+
+* 0-putchar.c - Write a program that prints _putchar, followed by a new line.
+
+
+## 0-putchar.c ##
+Write a program that prints _putchar, followed by a new line.
+* The program should return 0
+
+
