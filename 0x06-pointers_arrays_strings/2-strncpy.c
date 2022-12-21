956be4f3077f1f8349a04306cc2a7bc80bbec746
damiso <damiso15@yahoo.com>
2022-12-21 03:39:38 -0800

    strcmp

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,7 +5,7 @@
  * @dest: an input string
  * @src: an input string
  * @n: an input integer
- * Return: 
+ * Return: a pointer to the resulting dest string 
  */
 char *_strncpy(char *dest, char *src, int n)
 {
