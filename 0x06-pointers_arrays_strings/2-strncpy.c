bc79786fe5292e4834623d6aade2a06918952ba3
damiso <damiso15@yahoo.com>
2022-12-22 00:18:14 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,11 +5,11 @@
  * @dest: an input string
  * @src: an input string
  * @n: an input integer
- * Return: a pointer to the resulting dest string 
+ * Return: a pointer to the resulting dest string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int num =0;
+	int num = 0;
 
 	for (; num < n && src[num] != '\0'; num++)
 		dest[num] = src[num];
