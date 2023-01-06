e82f060bcf57c1aedd122af1a912200516d2cb66
damiso <damiso15@yahoo.com>
2023-01-06 13:16:57 -0800

    5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+
+/**
+ * amount - the main program
+ * @num: an input integer
+ * Return: the integer coins
+ */
+int amount(int num)
+{
+	int coins = 0, counter25 = 0, counter10 = 0;
+	int counter5 = 0, counter2 = 0, counter1 = 0;
+
+	while (num > 0)
+	{
+		if (num >= 25)
+		{
+			num -= 25;
+			counter25 += 1;
+		}
+		else if (num >= 10)
+		{
+			num -= 10;
+			counter10 += 1;
+		}
+		else if (num >= 5)
+		{
+			num -= 10;
+			counter2 += 1;
+		}
+		else if (num >= 2)
+		{
+			num -= 1;
+			counter1 += 1;
+		}
+	}
+	coins = counter25 + counter10 + counter5 + counter2 + counter1;
+	return (coins);
+}
 
 /**
  * main - a program that prints the minimum number of coins to make
@@ -12,54 +48,22 @@
  */
 int main(int argc, char *argv[])
 {
-	int change, coins, counter25, counter10;
-	int counter5, counter2, counter1;
+	int coins, change;
+	
+	change = atoi(argv[1]);
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	else if (change < 0)
+	{
+		printf("%d\n", 0);
+	}
 	else
 	{
-		change = atoi(argv[1]);
-		coins = 0;
-		counter25 = 0, counter10 = 0, counter5 = 0;
-		counter2 = 0, counter1 = 0;
-
-		while (change > 0)
-		{
-			if (change < 0)
-			{
-				printf("%d\n", 0);
-			}
-			else if (change >= 25)
-			{
-				change -= 25;
-				counter25 += 1;
-			}
-			else if (change >= 10)
-			{
-				change -= 10;
-				counter10 += 1;
-			}
-			else if (change >= 5)
-			{
-				change -= 5;
-				counter5 += 1;
-			}
-			else if (change >= 2)
-			{
-				change -= 2;
-				counter2 += 1;
-			}
-			else if (change >= 1)
-			{
-				change -= 1;
-				counter1 += 1;
-			}
-		}
-		coins = counter25 + counter10 + counter5 + counter2 + counter1;
+		coins = amount(change);
 		printf("%d\n", coins);
 	}
 	return (0);
