74a2dd41b38a7cd6ddb4abc036928bbe6d60cdcd
damiso <damiso15@yahoo.com>
2023-01-06 04:22:12 -0800

    0. A library is not a luxury but one of the necessities of life an 1. Without libraries what have we? We have no past and no future

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,12 @@
 #include "main.h"
 
+/**
+ * main - a function
+ * Return:  as success.
+ */
+
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
 }
