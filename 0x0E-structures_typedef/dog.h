41dc3fb911cdbee0e343483823e64c94914ae38f
damiso <damiso15@yahoo.com>
2023-01-16 03:57:04 -0800

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * dog - a new type struct dog
+ * @name: an element for the name of the dog
+ * @age: an element for the age of the dog
+ * @owner: an element for the owner of the dog
+ *
+ * Description: none
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
