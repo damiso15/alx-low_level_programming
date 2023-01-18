d331c577b0317d515204b28a909c905e36d05de1
damiso <damiso15@yahoo.com>
2023-01-18 10:04:09 -0800

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+
+/**
+ * main - the main function
+ * @argc: command-line argument count
+ * @argv: command-line argument in characters of array
+ * Return: 0 as Success otherwise 1 as Error
+ */
+int main(int argc, char *argv[])
+{
+	int (*result)(int, int), num1, num2;
+	char *sign;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]), num2 = atoi(argv[3]);
+	sign = argv[2];
+	result = get_op_func(sign);
+
+	if (result == NULL)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	printf("%d\n", result(num1, num2));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,69 @@
+#include "3-calc.h"
+
+/**
+ * op_add - a program that contains functions that performs addition
+ * @a: an input integer
+ * @b: an input integer
+ * Return: the sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - a program that contains functions that performs substraction
+ * @a: an input integer
+ * @b: an input integer
+ * Return: the sum of a and b
+ */
+int op_sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * op_mul - a program that contains functions that performs multiplication
+ * @a: an input integer
+ * @b: an input integer
+ * Return: the sum of a and b
+ */
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * op_div - a program that contains functions that performs division
+ * @a: an input integer
+ * @b: an input integer
+ * Return: the sum of a and b
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+	else
+        	return (a / b);
+}
+
+/**
+ * op_mod - a program that contains functions that performs modulo
+ * @a: an input integer
+ * @b: an input integer
+ * Return: the sum of a and b
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+	else
+	        return (a + b);
+}
+
