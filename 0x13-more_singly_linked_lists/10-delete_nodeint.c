0832c507ab0219b8be35f263380ca7fe086131d7
damiso <damiso15@yahoo.com>
2023-01-30 08:17:03 -0800

    10. Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,7 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if (counter == index)
 		{
 			new->next = temp->next;
-			free (temp);
+			free(temp);
 			return (1);
 		}
 		new = temp;
