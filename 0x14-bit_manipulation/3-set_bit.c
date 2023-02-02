4afaaf5c6ed9c4c4a169230c958de287e1f04218
damiso <damiso15@yahoo.com>
2023-02-02 10:02:49 -0800

    3. 11

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,7 +11,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int num;
 
 	if (index >= (sizeof(unsigned long int) * 8))
-			return (-1);
+		return (-1);
 
 	num = 1;
 	*n = *n | (num << index);
