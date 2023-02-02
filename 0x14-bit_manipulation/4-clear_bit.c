75a33c9261a09fe142b53e2a5a2202c2a0664547
damiso <damiso15@yahoo.com>
2023-02-02 10:15:05 -0800

    4. 100

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,7 +10,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int num;
 
-	if (index >= (sizeof (unsigned long int) * 8))
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
 	num = 1;
