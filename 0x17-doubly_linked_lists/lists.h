5a74a479f60d21564474f35d00b076599a6ed2d1
damiso <damiso15@yahoo.com>
2023-03-16 05:46:38 -0700

    0. Print list

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,36 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+
+/* Standard C Library */
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+
+
+/* Please use this data structure for this project */ 
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ *
+ */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+
+/* Prototype */
+size_t print_dlistint(const dlistint_t *h);
+
+#endif
